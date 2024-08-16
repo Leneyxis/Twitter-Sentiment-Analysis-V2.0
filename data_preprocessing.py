fc9c156ee19d201b4126aabefa76be335b8ae214
Syedumer97 <78430937+Leneyxis@users.noreply.github.com>
2024-08-16 18:22:50 +0530

    Updates

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import re
 import os
-import matplotlib.pyplot as plt
-
 
 #Read and combine the Dataset.
 def combine_datasets()->'csv': # type: ignore
     df = pd.read_csv('Dataset1.csv', encoding='utf-8') 
     statements=df['comment'].to_list()
-    df = pd.read_csv('Twitter_Data.csv', encoding='utf-8')
-    statements.extend(df['clean_text'].to_list())
-    df = pd.read_csv('Reddit_Data.csv', encoding='utf-8')
-    statements.extend(df['clean_comment'].to_list())
     df = pd.DataFrame(statements,columns=['Tweets'])
     df.to_csv('Final_Dataset.csv')
+
+
+#Preprocess the Dataset. Remove url, mentions, and hashtags.
+def preprocessing()->'csv': # type: ignore
+    df = pd.read_csv('Final_Dataset.csv', encoding='utf-8')
+    df['Tweets'] = df['Tweets'].astype(str)
+    df['Tweets'] = df['Tweets'].apply(lambda x: re.sub(r"http\S+|@\S+|#\S+", "", x))
+    df=df.dropna()
+    df.to_csv('Final_Dataset.csv',index=False)
+
 
 def main():
     if os.path.isfile('Final_Dataset.csv') == False:
         combine_datasets()
+        preprocessing()
+    return("Preprocessing Done")
 
 main()
---
+++ sentiment_analysis.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import seaborn as sb
+import matplotlib.pyplot as plt
